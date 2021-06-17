f913268dbb219bbd3f4db48063af902aa3ff0313
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-17 19:37:54 +0430

    show number of list item

--- src/Components/App.js
+++ src/Components/App.js
@@ -47,9 +47,9 @@ class App extends Component {
                                 <nav className="col-6 mb-3">
                                     <div className="nav nav-tabs" id="nav-tab" role="tablist">
                                         <a className={`nav-item nav-link font-weight-bold ${ !statusDone ? 'active' : '' }`}
-                                           id="nav-home-tab" onClick={() => this.setState({ statusDone: false })}>undone <span className="badge badge-secondary">9</span></a>
+                                           id="nav-home-tab" onClick={() => this.setState({ statusDone: false })}>undone <span className="badge badge-secondary">{todos.filter(item => item.done == false).length}</span></a>
                                         <a className={`nav-item nav-link active font-weight-bold ${ statusDone ? 'active' : '' }`}
-                                           id="nav-profile-tab" onClick={() => this.setState({ statusDone: true })}>done <span className="badge badge-success">9</span></a>
+                                           id="nav-profile-tab" onClick={() => this.setState({ statusDone: true })}>done <span className="badge badge-success">{todos.filter(item => item.done == true).length}</span></a>
                                     </div>
                                 </nav>
                                 {
