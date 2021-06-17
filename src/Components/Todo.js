9bdcd58f60a41985b47c4ae2aa6f52081885fefc
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-17 23:14:57 +0430

    fixed btn color

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,15 +2,18 @@ import React from "react";
 
 
 function Todo (props) {
+
+    const { item } = props;
+
     return (
         <div className="col-6 mb-2">
             <div
                 className="d-flex justify-content-between align-items-center border rounded p-3">
                 <div>
-                    {props.item.text}
+                    {item.text}
                 </div>
                 <div>
-                    <button type="button" className="btn btn-success btn-sm mr-1" onClick={()=>props.done(props.item.key)}>{props.item.done ? 'undone' : 'done'}</button>
+                    <button type="button" className={`btn btn-sm mr-1 ${item.done ? 'btn-warning' : 'btn-success' }`} onClick={()=>props.done(props.item.key)}>{props.item.done ? 'undone' : 'done'}</button>
                     <button type="button" className="btn btn-info btn-sm mr-1">edit</button>
                     <button type="button" className="btn btn-danger btn-sm" onClick={()=>props.delete(props.item.key)}>delete</button>
                 </div>
