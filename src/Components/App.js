c4afe34e15323285b2adb8080e4852c28ba5a33c
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-17 18:02:54 +0430

    add Todo file

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,6 +2,7 @@ import React , { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import Header from "./header";
 import FormAddTodo from "./FormAddTodo";
+import Todo from "./Todo";
 
 
 class App extends Component {
@@ -44,42 +45,7 @@ class App extends Component {
                                            id="nav-profile-tab">done <span className="badge badge-success">9</span></a>
                                     </div>
                                 </nav>
-                                <div className="col-6 mb-2">
-                                    <div
-                                        className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
-                                        </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-6 mb-2">
-                                    <div
-                                        className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
-                                        </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-6 mb-2">
-                                    <div
-                                        className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
-                                        </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
+                                <Todo />
                             </div>
 
                         </div>
