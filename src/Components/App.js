d26f08d4f6e7d6ea5005d0257ba86909249c1a4b
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-17 12:02:47 +0430

    test App file

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,7 +2,11 @@ import react , { Component } from 'react';
 
 
 class App extends Component {
-    
+    render() {
+        return(
+            <h2>Hello Maryam</h2>
+        )
+    }
 }
 
 export default App;
