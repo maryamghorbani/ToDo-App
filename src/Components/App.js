96c791a56506dcc9013b0e731fa99cf82f11102a
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-18 16:41:10 +0430

    create TodoList file

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Header from "./Layout/header";
 import FormAddTodo from "./Todo/FormAddTodo";
 import Todo from "./Todo/Todo";
+import TodoList from "./Todo/TodoList";
 
 
 class App extends Component {
@@ -82,25 +83,7 @@ class App extends Component {
                     <div className="todosList">
                         <div className="container">
                             <div className="d-flex flex-column align-items-center ">
-                                <nav className="col-6 mb-3">
-                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                                        <a className={`nav-item nav-link font-weight-bold ${ !statusDone ? 'active' : '' }`}
-                                           id="nav-home-tab" onClick={() => this.setState({ statusDone: false })}>undone <span className="badge badge-secondary">{todos.filter(item => item.done == false).length}</span></a>
-                                        <a className={`nav-item nav-link active font-weight-bold ${ statusDone ? 'active' : '' }`}
-                                           id="nav-profile-tab" onClick={() => this.setState({ statusDone: true })}>done <span className="badge badge-success">{todos.filter(item => item.done == true).length}</span></a>
-                                    </div>
-                                </nav>
-                                {
-                                    filterTodos == 0
-                                        ? <p>There isn't any todos!</p>
-                                        : filterTodos.map(item => <Todo key={item.key}
-                                                                        item={item}
-                                                                        delete={this.deleteTodo.bind(this)}
-                                                                        done={this.toggleTodo.bind(this)}
-                                                                        edit={this.editTodo.bind(this)}
-                                                                        />
-                                                                        )
-                                }
+                                <TodoList />
                             </div>
 
                         </div>
