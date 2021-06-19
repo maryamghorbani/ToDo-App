bf72e65b9168926f2f7b3169d5c823c5f563a67c
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-19 14:56:24 +0430

    edit app.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -82,17 +82,13 @@ class App extends Component {
                             <div className="container d-flex flex-column align-items-center">
                                 <h1 className="jumbotron-heading">Welcome!</h1>
                                 <p className="lead text-muted">To get started, add some items to your list:</p>
-                                <FormAddTodo add={this.addTodo.bind(this)} />
+                                <FormAddTodo />
                             </div>
                         </section>
                         <div className="todosList">
                             <div className="container">
                                 <div className="d-flex flex-column align-items-center ">
-                                    <TodoList todos={this.state.todos}
-                                              delete={this.deleteTodo.bind(this)}
-                                              done={this.toggleTodo.bind(this)}
-                                              edit={this.editTodo.bind(this)}
-                                    />
+                                    <TodoList />
                                 </div>
 
                             </div>
