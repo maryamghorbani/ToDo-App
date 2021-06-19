450d81e8fbd2bbd4cf501dc662cc0f4f230f4d9d
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-19 12:52:53 +0430

    add values of context

--- src/Context/todos.js
+++ src/Context/todos.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const TodosContext = React.createContext({})
+const TodosContext = React.createContext({
+    todos : [],
+    add : () => {},
+    done : () => {},
+    delete : () => {},
+    edit : () => {}
+})
 
 export default TodosContext;
