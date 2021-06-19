29281218b7f014c6effd8a55d2516af3cc43cd67
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-19 12:45:47 +0430

    add TodosContext component

--- src/Components/App.js
+++ src/Components/App.js
@@ -68,30 +68,32 @@ class App extends Component {
     render() {
 
         return(
-            <div className="App">
-                <Header />
-                <main>
-                    <section className="jumbotron">
-                        <div className="container d-flex flex-column align-items-center">
-                            <h1 className="jumbotron-heading">Welcome!</h1>
-                            <p className="lead text-muted">To get started, add some items to your list:</p>
-                            <FormAddTodo add={this.addTodo.bind(this)} />
-                        </div>
-                    </section>
-                    <div className="todosList">
-                        <div className="container">
-                            <div className="d-flex flex-column align-items-center ">
-                                <TodoList todos={this.state.todos}
-                                          delete={this.deleteTodo.bind(this)}
-                                          done={this.toggleTodo.bind(this)}
-                                          edit={this.editTodo.bind(this)}
-                                />
+            <TodosContext.Provider value={this.state}>
+                <div className="App">
+                    <Header />
+                    <main>
+                        <section className="jumbotron">
+                            <div className="container d-flex flex-column align-items-center">
+                                <h1 className="jumbotron-heading">Welcome!</h1>
+                                <p className="lead text-muted">To get started, add some items to your list:</p>
+                                <FormAddTodo add={this.addTodo.bind(this)} />
                             </div>
+                        </section>
+                        <div className="todosList">
+                            <div className="container">
+                                <div className="d-flex flex-column align-items-center ">
+                                    <TodoList todos={this.state.todos}
+                                              delete={this.deleteTodo.bind(this)}
+                                              done={this.toggleTodo.bind(this)}
+                                              edit={this.editTodo.bind(this)}
+                                    />
+                                </div>
 
+                            </div>
                         </div>
-                    </div>
-                </main>
-            </div>
+                    </main>
+                </div>
+            </TodosContext.Provider>
         )
     }
 }
