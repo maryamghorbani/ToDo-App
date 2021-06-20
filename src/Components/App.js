251879e365a3f3e5b266e149860182db5ec3aa4b
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-20 13:54:07 +0430

    change App.js from class base component to functional component

--- src/Components/App.js
+++ src/Components/App.js
@@ -11,66 +11,117 @@ import TodosContext from "../Context/todos";
 import AuthContext from "../Context/auth";
 
 
-class App extends Component {
-    state = {
-        todos : [],
-        authenticated : false
-    }
+// class App extends Component {
+//     state = {
+//         todos : [],
+//         authenticated : false
+//     }
+//
+//     addTodo(text) {
+//         this.setState(prevState => {
+//             return {
+//
+//                 todos : [
+//                     ... prevState.todos,
+//                     { key : Date.now() , done : false , text }
+//                 ]
+//             }
+//         })
+//     }
+//
+//     deleteTodo(key) {
+//         this.setState(prevSate => {
+//             return {
+//                 todos : prevSate.todos.filter(item => item.key !== key)
+//             }
+//         })
+//     }
+//
+//     toggleTodo(key) {
+//         let { todos } = this.state;
+//         let item = todos.find(item => item.key == key)
+//         item.done = ! item.done;
+//
+//         let newTodos = todos.filter(item => item.key !== key)
+//
+//         this.setState({
+//             todos: [
+//                 ... newTodos,
+//                 item
+//             ]
+//         })
+//     }
+//
+//     editTodo(key , text) {
+//         let { todos } = this.state;
+//         let item = todos.find(item => item.key == key)
+//         item.text = text;
+//
+//         let newTodos = todos.filter(item => item.key !== key)
+//
+//         this.setState({
+//             todos: [
+//                 ... newTodos,
+//                 item
+//             ]
+//         })
+//     }
+//
+//     render() {
+//
+//         return(
+//
+//             <AuthContext.Provider value={{
+//                 authenticated: this.state.authenticated,
+//                 login : () => { this.setState({ authenticated : true }) },
+//                 logout : () => { this.setState({ authenticated : false }) }
+//             }
+//             }>
+//                 <TodosContext.Provider value={{
+//                     todos : this.state.todos,
+//                     add : this.addTodo.bind(this),
+//                     done : this.toggleTodo.bind(this),
+//                     delete : this.deleteTodo.bind(this),
+//                     edit : this.editTodo.bind(this)
+//                 }}>
+//                     <div className="App">
+//                         <Header />
+//                         <main>
+//                             <section className="jumbotron">
+//                                 <div className="container d-flex flex-column align-items-center">
+//                                     <h1 className="jumbotron-heading">Welcome!</h1>
+//                                     <p className="lead text-muted">To get started, add some items to your list:</p>
+//                                     <FormAddTodo />
+//                                 </div>
+//                             </section>
+//                             <div className="todosList">
+//                                 <div className="container">
+//                                     <div className="d-flex flex-column align-items-center ">
+//                                         <TodoList />
+//                                     </div>
+//
+//                                 </div>
+//                             </div>
+//                         </main>
+//                     </div>
+//                 </TodosContext.Provider>
+//             </AuthContext.Provider>
+//
+//         )
+//     }
+// }
 
-    addTodo(text) {
-        this.setState(prevState => {
-            return {
 
-                todos : [
-                    ... prevState.todos,
-                    { key : Date.now() , done : false , text }
-                ]
-            }
-        })
-    }
+function App() {
 
-    deleteTodo(key) {
-        this.setState(prevSate => {
-            return {
-                todos : prevSate.todos.filter(item => item.key !== key)
-            }
-        })
-    }
 
-    toggleTodo(key) {
-        let { todos } = this.state;
-        let item = todos.find(item => item.key == key)
-        item.done = ! item.done;
+    // state = {
+    //     todos : [],
+    //     authenticated : false
+    // }
 
-        let newTodos = todos.filter(item => item.key !== key)
 
-        this.setState({
-            todos: [
-                ... newTodos,
-                item
-            ]
-        })
-    }
-
-    editTodo(key , text) {
-        let { todos } = this.state;
-        let item = todos.find(item => item.key == key)
-        item.text = text;
-
-        let newTodos = todos.filter(item => item.key !== key)
-
-        this.setState({
-            todos: [
-                ... newTodos,
-                item
-            ]
-        })
-    }
-
-    render() {
-
-        return(
-
+    return(
             <AuthContext.Provider value={{
                 authenticated: this.state.authenticated,
                 login : () => { this.setState({ authenticated : true }) },
@@ -108,7 +159,6 @@ class App extends Component {
             </AuthContext.Provider>
 
         )
-    }
 }
 
 export default App;
--- src/Reducers/appReducer.js
+++ src/Reducers/appReducer.js
@@ -2,3 +2,57 @@ function AppReducer(prevState , action) {
     console.log(prevState,action);
     return{};
 }
+
+
+
+
+//     addTodo(text) {
+//         this.setState(prevState => {
+//             return {
+//
+//                 todos : [
+//                     ... prevState.todos,
+//                     { key : Date.now() , done : false , text }
+//                 ]
+//             }
+//         })
+//     }
+//
+//     deleteTodo(key) {
+//         this.setState(prevSate => {
+//             return {
+//                 todos : prevSate.todos.filter(item => item.key !== key)
+//             }
+//         })
+//     }
+//
+//     toggleTodo(key) {
+//         let { todos } = this.state;
+//         let item = todos.find(item => item.key == key)
+//         item.done = ! item.done;
+//
+//         let newTodos = todos.filter(item => item.key !== key)
+//
+//         this.setState({
+//             todos: [
+//                 ... newTodos,
+//                 item
+//             ]
+//         })
+//     }
+//
+//     editTodo(key , text) {
+//         let { todos } = this.state;
+//         let item = todos.find(item => item.key == key)
+//         item.text = text;
+//
+//         let newTodos = todos.filter(item => item.key !== key)
+//
+//         this.setState({
+//             todos: [
+//                 ... newTodos,
+//                 item
+//             ]
+//         })
+//     }
+//
