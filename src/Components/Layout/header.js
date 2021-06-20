0b51577942aca2c60963eef37db973fa01946497
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-20 11:56:02 +0430

    add login btn to header

--- src/Components/Layout/header.js
+++ src/Components/Layout/header.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React , { useContext} from 'react';
+import AuthContext from "../../Context/auth";
 
 
 function Header() {
+
+
+    const authContext = useContext(AuthContext);
+
+
     return (
         <header>
-            <div className="navbar navbar-dark bg-dark shadow-sm">
+            <div className="navbar navbar-dark bg-dark shadowh-sm">
                 <div className="container d-flex justify-content-between">
                     <strong className="navbar-brand d-flex align-items-center">Todo App</strong>
+                    <button className="btn btn-sm btn-success" onClick={doLogin}>login</button>
                 </div>
             </div>
         </header>
