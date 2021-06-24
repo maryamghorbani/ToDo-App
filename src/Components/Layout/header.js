06dfc887a5cf892806398cf58375853f05de6032
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-24 19:24:46 +0430

    create navbar

--- src/Components/Layout/header.js
+++ src/Components/Layout/header.js
@@ -15,9 +15,24 @@ function Header() {
 
     return (
         <header>
-            <div className="navbar navbar-dark bg-dark shadowh-sm">
+            <div className="navbar navbar-dark navbar-expand-md bg-dark shadowh-sm">
                 <div className="container d-flex justify-content-between">
                     <strong className="navbar-brand d-flex align-items-center">Todo App</strong>
+
+
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="/">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/about">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/contact">Contact</a>
+                        </li>
+                    </ul>
+
+
                     {
                         ! authContext.authenticated
                         ? <button className="btn btn-sm btn-success" onClick={doLogin}>login</button>
