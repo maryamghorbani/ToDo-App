cecd370c4eb5c7fd37260b9a495e8a441e9b6097
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-25 20:01:33 +0430

    change Todo route from class base component to functional base component

--- src/Routs/Todo.js
+++ src/Routs/Todo.js
@@ -1,12 +1,10 @@
 import React from 'react';
 
-class Todo extends React.Component {
-    render() {
-        console.log(this.props.match.params);
-        return (
-            <h2> Todo Details </h2>
-        )
-    }
+function Todo() {
+    console.log(this.props.match.params);
+    return (
+        <h2> Todo Details </h2>
+    )
 }
 
 export default Todo;
