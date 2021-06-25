94b7745f0dc20fa30f264c03db58d144910544e2
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-06-25 20:32:30 +0430

    create loading for data load

--- src/Routs/Todo.js
+++ src/Routs/Todo.js
@@ -7,10 +7,13 @@ import todoApi from "../Api/todos";
 function Todo(props) {
     const params = useParams();
     const [todo , setTodo] = useState({});
+    const [loading , setLoading] = useState();
 
     useEffect(() => {
+        setLoading(true);
         todoApi.get(`/todos/${params.todo}.json`)
             .then( response => {
+                setLoading(false);
                 setTodo({ ...response.data , key : params.todo })
             })
             .catch( err => console.log(err) )
@@ -20,9 +23,17 @@ function Todo(props) {
         <div className="container">
             <div className="row">
                 <div className="col-12">
-                    <h2> Todo Detail </h2>
-                    <p>{todo.text}</p>
-                    <span className={`badge ${todo.done ? 'badge-success' : 'badge-warning'}`}>{ todo.done ? 'done' : 'undone' }</span>
+                    {
+                        loading
+                            ? <h2>Data is Loading ...</h2>
+                            : (
+                                <>
+                                    <h2> Todo Detail </h2>
+                                    <p>{todo.text}</p>
+                                    <span className={`badge ${todo.done ? 'badge-success' : 'badge-warning'}`}>{ todo.done ? 'done' : 'undone' }</span>
+                                </>
+                            )
+                    }
                 </div>
             </div>
         </div>
